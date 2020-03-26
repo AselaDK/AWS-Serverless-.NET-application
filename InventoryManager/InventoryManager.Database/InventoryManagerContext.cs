1ecfcf8dbe71a0c90e6438b16aa73bc1d96b1cc5
aseladk <38081311+AselaDK@users.noreply.github.com>
2020-03-26 14:29:28 +0530

    Database migration is done.

---
+++ InventoryManager/InventoryManager.Database/InventoryManagerContext.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManager.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManager.Database
+{
+    public class InventoryManagerContext: DbContext
+    {
+        // name is Pallets
+        public virtual DbSet<Pallet> Pallets { get; set; }
+
+        // override onconfiguring method to connect database
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // to check the same operation is done only one time
+            if (optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+            //same uname password as serverless.template
+            optionsBuilder.UseSqlServer($"Server=iddg56telmtadz.cmxxgvnwvplv.ap-south-1.rds.amazonaws.com; Database=InventoryManagerDB; User Id = yourusername; Password=youruserpassword");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ InventoryManager/InventoryManager.Database/Migrations/20200325125344_Initial.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryManager.Database.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Size",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Width = table.Column<int>(nullable: false),
+                    Height = table.Column<int>(nullable: false),
+                    Length = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Size", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pallets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SizeId = table.Column<int>(nullable: true),
+                    Weight = table.Column<decimal>(nullable: false),
+                    IsPlaced = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pallets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pallets_Size_SizeId",
+                        column: x => x.SizeId,
+                        principalTable: "Size",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pallets_SizeId",
+                table: "Pallets",
+                column: "SizeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pallets");
+
+            migrationBuilder.DropTable(
+                name: "Size");
+        }
+    }
+}
